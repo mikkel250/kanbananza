cf0ee7deb4676f1bacb0d8250cbcc38639d234d9
Mikkel Ridley <mikkel250@gmail.com>
2020-03-28 18:09:17 -0700

    begin wiring up redux to react

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
+import rootReducer from './reducers';
 import Application from './components/Application';
 
 import './index.scss';
 
-ReactDOM.render(<Application />, document.getElementById('root'));
+const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__()); // create the store and pass it the reducers. Second arg is to enable redux devtools
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Application />
+  </Provider>,
+  document.getElementById('root'),
+);
--- src/normalized-state.js
+++ src/normalized-state.js
@@ -1,13 +1,13 @@
 // this normalizes the data so you can get it in the shape that you want. See the video linked here and the one before it
 // https://frontendmasters.com/courses/redux-mobx/wiring-state-store-to-the-app/
 
-import { schema, normalize } from "normalizr";
-import defaultState from "./default-state";
+import { schema, normalize } from 'normalizr';
+import defaultState from './default-state';
 
-const user = new schema.Entity("users");
-const card = new schema.Entity("cards", { assignTo: user });
-const list = new schema.Entity("lists", {
-  cards: [card]
+const user = new schema.Entity('users');
+const card = new schema.Entity('cards', { assignedTo: user });
+const list = new schema.Entity('lists', {
+  cards: [card],
 });
 
 const normalizedLists = normalize(defaultState.lists, [list]);
@@ -15,17 +15,17 @@ const normalizedUsers = normalize(defaultState.users, [user]);
 
 export const lists = {
   entities: normalizedLists.entities.lists,
-  ids: normalizedLists.result
+  ids: normalizedLists.result,
 };
 
 export const users = {
-  entities: normalizedUsers.entities.lists,
-  ids: normalizedUsers.result
+  entities: normalizedUsers.entities.users,
+  ids: normalizedUsers.result,
 };
 
 export const cards = {
   entities: normalizedLists.entities.cards,
-  ids: Object.keys(normalizedLists.entities.cards)
+  ids: Object.keys(normalizedLists.entities.cards),
 };
 
 export default { lists, users, cards };
